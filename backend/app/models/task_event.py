7439844af76f4b717cff79c20a078248d42ccb42
yofiyamariraja@gmail.com <yofiyamariraja@gmail.com>
2026-04-11 15:07:52 +0530

    time format issue fix

--- backend/app/models/task_event.py
+++ backend/app/models/task_event.py
@@ -1,15 +1,18 @@
 from typing import Optional
 from datetime import datetime
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, DateTime
 
 class TaskEvent(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
-    taskName: str  # Collected | Started | Reached | Handed Over
-    timestamp: datetime
+    taskName: str
+    timestamp: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), nullable=False)
+    )
     latitude: float
     longitude: float
     location: Optional[str] = None
 
-    class Config:        # used in case of custom datatype i.e. type not present in Pydantic
+    class Config:
         arbitrary_types_allowed = True
