32116f3f9a9ab4a2c2fad1fad93722ff0c77e342
yofiyamariraja@gmail.com <yofiyamariraja@gmail.com>
2026-04-16 20:17:21 +0530

    map issue fix

--- backend/app/api/gps.py
+++ backend/app/api/gps.py
@@ -16,7 +16,7 @@ def location_ping(
     session: Session = Depends(get_session)
 ):
     ping = GPSPing(
-        userId=data.user_id,
+        userId=data.userId,
         timestamp=data.timestamp,
         latitude=data.latitude,
         longitude=data.longitude,
